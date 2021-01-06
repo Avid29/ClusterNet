3c40e0049f24033e3f5c3d7f5105cc0a6dae0b87
Adam Dernis <avid29@live.com>
2021-01-06 10:45:32 -0500

    Gaussian Kernel

---
+++ UnitTests/Mean1D/MeanShift1DTests.cs
@@ -0,0 +1,67 @@
+﻿using ClusterLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTests.Mean1D
+{
+    [TestClass]
+    public class MeanShift1DTests
+    {
+        [TestMethod]
+        public void Flat1DTest1()
+        {
+            List<double> points = new List<double>()
+            {
+                0,
+                1,
+                8,
+                10,
+                12,
+                22,
+                24,
+            };
+
+            List<double> expectedClusters = new List<double>()
+            {
+                .5d,
+                10d,
+                23d,
+            };
+
+            var clusters = ClusterMethods.MeanShift<double, FlatDoubleShape>(points, 5)
+                .Select(x => x.Centroid).ToList();
+
+            CollectionAssert.AreEquivalent(expectedClusters, clusters);
+        }
+
+        [TestMethod]
+        public void Gaussian1DTest1()
+        {
+            List<double> points = new List<double>()
+            {
+                0,
+                1,
+                8,
+                10,
+                12,
+                22,
+                24,
+            };
+
+            List<double> expectedClusters = new List<double>()
+            {
+                .5d,
+                10d,
+                23d,
+            };
+
+            var clusters = ClusterMethods.MeanShift<double, GaussianDoubleShape>(points, 5)
+                .Select(x => x.Centroid).ToList();
+
+            CollectionAssert.AreEquivalent(expectedClusters, clusters);
+        }
+    }
+}
