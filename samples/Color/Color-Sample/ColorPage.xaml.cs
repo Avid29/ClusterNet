9a7e1fd0239df18415a2f9ac29447a25f905189e
Adam Dernis <avid29@live.com>
2021-01-07 02:49:08 -0500

    Color Sample app start

---
+++ samples/Color/Color-Sample/ColorPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Color_Sample
+{
+    public sealed partial class ColorPage : UserControl
+    {
+        private string imageUrl;
+        
+        public ColorPage(string url)
+        {
+            this.InitializeComponent();
+            imageUrl = url;
+
+            this.Loaded += ColorPage_Loaded;
+        }
+
+        private void ColorPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            this.Loaded -= ColorPage_Loaded;
+            
+            Image.Source = new BitmapImage(new Uri(imageUrl));
+        }
+    }
+}
---
+++ samples/Color/Color-Sample/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Color_Sample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void AddBlade(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ColorsBladeView.Items.Add(new ColorPage(UriTextBox.Text));
+        }
+    }
+}
