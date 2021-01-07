f1c06d41d04aea0ad47497bbe90cc6775d3c8353
Adam Dernis <avid29@live.com>
2021-01-07 11:10:42 -0500

    Comments for IPoint shape

--- src/Cluster/Shapes/IPoint.cs
+++ src/Cluster/Shapes/IPoint.cs
@@ -1,13 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using ClusterLib.Abstract;
+using System.Collections.Generic;
 
 namespace ClusterLib.Shapes
 {
+    /// <summary>
+    /// A Shape for the Point in a <see cref="Cluster{T, TShape}"/>.
+    /// </summary>
+    /// <typeparam name="T">The type being wrapped by the implementation.</typeparam>
     public interface IPoint<T>
     {
+        /// <summary>
+        /// Gets the average value of a list of <typeparamref name="T"/> items.
+        /// </summary>
+        /// <param name="items">The list of points to average.</param>
+        /// <returns>The average of all items in the enumerable.</returns>
         T Average(IEnumerable<T> items);
 
-        T WeightedAverage(IEnumerable<(T, double)> items);
-
+        /// <summary>
+        /// Gets the distance between <paramref name="it1"/> and <paramref name="it2"/>.
+        /// </summary>
+        /// <param name="it1">Point A.</param>
+        /// <param name="it2">Point B.</param>
+        /// <returns>The distance between point A and point B.</returns>
         double FindDistanceSquared(T it1, T it2);
+
+        /// <summary>
+        /// Gets the weighted average value of a list of (T, double) by point and weight.
+        /// </summary>
+        /// <param name="items">A weighted list of points.</param>
+        /// <returns>The weighted center of the points.</returns>
+        T WeightedAverage(IEnumerable<(T, double)> items);
     }
 }
