4e8ed5b36a34af968d6e9ee29eca55f334d90947
Adam Dernis <avid29@live.com>
2021-01-09 17:01:46 -0500

    Added more images to MeanShift Benchmarks

--- tests/Benchmarks/Benchmarks/MeanShift/Colors/MeanShiftRGBBenchmarks.cs
+++ tests/Benchmarks/Benchmarks/MeanShift/Colors/MeanShiftRGBBenchmarks.cs
@@ -12,20 +12,49 @@ namespace Benchmarks.MeanShift.Colors
     public class MeanShiftRGBBenchmarks
     {
         private List<RGBColor> colors;
+        private Dictionary<string, string> nameToImage;
 
-        [Params(256, 512)]
-        public int Quality;
-
-        [Params(.05, .1, .2, .5)]
-        public double Bandwidth;
-
-        [Params("https://i.pinimg.com/736x/b6/63/a5/b663a51354e2435219997157d33f2a77--the-eagles-vinyl-records.jpg")]
+        [Params(
+            "Nirvana - Nevermind",
+            "Vampie Weekend - Contra",
+            "Pink Floyd - Time",
+            "The Beatles - Revolver",
+            "Band of Horses - Everything All The Time",
+            "C418 - Minecraft - Volume Alpha",
+            "Red Hot Chili Peppers - Califorinaction",
+            "The Strokes - Is This It")]
         public string Url;
 
+        [Params(1920)]
+        public int PixelCount;
+
+        [Params(480)]
+        public int Quality;
+
+        //[Params(.05, .1, .2, .5)]
+        [Params(.15)]
+        public double Bandwidth;
+
         [GlobalSetup]
-        public void Setup()
+        public void GlobalSetup()
         {
-            var image = ImageParser.GetImage(Url).GetAwaiter().GetResult(); ;
+            nameToImage = new Dictionary<string, string>()
+            {
+                { "Nirvana - Nevermind", "https://th.bing.com/th/id/OIP.eJ971LxYKRGqfkPXbzRkGAHaHa?pid=Api&rs=1" },
+                { "Vampie Weekend - Contra", "https://th.bing.com/th/id/OIP.RgT_6lZp_G-peHs97hqMNAHaHa?pid=Api&rs=1" },
+                { "Pink Floyd - Time", "https://s-media-cache-ak0.pinimg.com/736x/70/7c/98/707c98df5d2cffde6d4f755e3008771b.jpg" },
+                { "The Beatles - Revolver", "https://neonmoderntimes.files.wordpress.com/2014/07/beatles-revolver-cover-art.jpg" },
+                { "Band of Horses - Everything All The Time", "https://fanart.tv/fanart/music/07b6020a-c539-4d68-aeef-f159f3befc76/albumcover/everything-all-the-time-52c5e32f129ab.jpg" },
+                { "C418 - Minecraft - Volume Alpha", "https://f4.bcbits.com/img/a3390257927_10.jpg" },
+                { "Red Hot Chili Peppers - Califorinaction", "https://th.bing.com/th/id/OIP.F21MZzafrllhDLK-SZ4jhQHaHW?pid=Api&rs=1" },
+                { "The Strokes - Is This It", "https://media.pitchfork.com/photos/5929a58b13d1975652138f9b/1:1/w_600/c1b895b7.jpg" },
+            };
+        }
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            var image = ImageParser.GetImage(nameToImage[Url]).GetAwaiter().GetResult();
 
             if (image is null)
                 return;
@@ -33,18 +62,18 @@ namespace Benchmarks.MeanShift.Colors
             colors = ImageParser.GetImageColors(image, Quality);
         }
 
-        [Benchmark]
-        public void Flat()
-        {
-            FlatKernel kernel = new FlatKernel(Bandwidth);
-            MeanShiftMethod.MeanShift<RGBColor, RGBShape>(colors, kernel);
-        }
+        //[Benchmark]
+        //public void Flat()
+        //{
+        //    FlatKernel kernel = new FlatKernel(Bandwidth);
+        //    MeanShiftMethod.MeanShift<RGBColor, RGBShape>(colors, kernel);
+        //}
 
         [Benchmark]
         public void Gaussian()
         {
             GaussianKernel kernel = new GaussianKernel(Bandwidth);
-            MeanShiftMethod.MeanShift<RGBColor, RGBShape>(colors, kernel);
+            MeanShiftMethod.MeanShift<RGBColor, RGBShape>(colors, kernel, Quality);
         }
     }
 }
