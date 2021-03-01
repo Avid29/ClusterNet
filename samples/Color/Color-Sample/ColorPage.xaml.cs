cb915a662d62daab7da33050a3463025b7edf56c
Adam Dernis <avid29@live.com>
2021-03-01 13:54:17 -0500

    Changed sample to use WeightedMeanShift

--- samples/Color/Color-Sample/ColorPage.xaml.cs
+++ samples/Color/Color-Sample/ColorPage.xaml.cs
@@ -64,7 +64,7 @@ namespace Color_Sample
 
             RGBColor[] rgbColors = ImageParser.GetImageColors(image, 1920);
             GaussianKernel kernel = new GaussianKernel(.15);
-            (RGBColor, int)[] clusters = ClusterAlgorithms.MeanShift<RGBColor, RGBShape, GaussianKernel>(rgbColors, kernel, 480);
+            (RGBColor, int)[] clusters = ClusterAlgorithms.WeightedMeanShift<RGBColor, RGBShape, GaussianKernel>(rgbColors, kernel, 480);
 
             List<(Color, int)> weightedColors = clusters.Select(x =>
             {
