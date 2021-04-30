1da6d204198aebcaa989bad4cfe7d5331e32f81b
Adam Dernis <avid29@live.com>
2021-04-30 17:38:17 -0400

    Added KMeans benchmark

---
+++ tests/Benchmarks/KMeans-Benchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using Benchmarks.KMeans.Colors;
+
+namespace Benchmarks.KMeans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<KMeansColorBenchmarks>();
+        }
+    }
+}
