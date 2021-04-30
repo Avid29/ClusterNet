58909f7e07dac7af12e32a889a5ee4585c043a18
Adam Dernis <avid29@live.com>
2021-04-30 15:56:25 -0400

    Added tests for MeanShift variants

---
+++ tests/UnitTests/MeanShift-Tests/MeanShiftMultiThreaded.cs
@@ -0,0 +1,40 @@
+﻿using ClusterNet;
+using ClusterNet.Kernels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Numerics;
+using Tests.Shapes;
+
+namespace Tests.MeanShift
+{
+    [TestClass]
+    public class MeanShiftMultiThreaded
+    {
+        [TestMethod]
+        public void DoubleTest1Flat()
+        {
+            FlatKernel kernel = new FlatKernel(DoubleTests.DoubleTest1.Bandwidth);
+            ClusterAlgorithms.MeanShiftMultiThreaded<double, DoubleShape, FlatKernel>(DoubleTests.DoubleTest1.Input, kernel);
+        }
+
+        [TestMethod]
+        public void Vector2Test1Flat()
+        {
+            FlatKernel kernel = new FlatKernel(Vector2Tests.Vector2Test1.Bandwidth);
+            ClusterAlgorithms.MeanShiftMultiThreaded<Vector2, Vector2Shape, FlatKernel>(Vector2Tests.Vector2Test1.Input, kernel);
+        }
+
+        [TestMethod]
+        public void DoubleTest1Gaussian()
+        {
+            GaussianKernel kernel = new GaussianKernel(DoubleTests.DoubleTest1.Bandwidth);
+            ClusterAlgorithms.MeanShiftMultiThreaded<double, DoubleShape, GaussianKernel>(DoubleTests.DoubleTest1.Input, kernel);
+        }
+
+        [TestMethod]
+        public void Vector2Test1Gaussian()
+        {
+            GaussianKernel kernel = new GaussianKernel(Vector2Tests.Vector2Test1.Bandwidth);
+            ClusterAlgorithms.MeanShiftMultiThreaded<Vector2, Vector2Shape, GaussianKernel>(Vector2Tests.Vector2Test1.Input, kernel);
+        }
+    }
+}
