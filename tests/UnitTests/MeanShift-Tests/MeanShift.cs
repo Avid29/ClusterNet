893dc9eb7db4e5158aa5fbabda92f57c6a024570
Adam Dernis <avid29@live.com>
2021-05-11 15:41:01 -0400

    Reverted guarenteed assertion

--- tests/UnitTests/MeanShift-Tests/MeanShift.cs
+++ tests/UnitTests/MeanShift-Tests/MeanShift.cs
@@ -39,7 +39,6 @@ namespace Tests.MeanShift
         [TestMethod]
         public void Vector2_Test1_Gaussian()
         {
-            Assert.Fail();
             var test = Vector2Tests.Vector2Test1;
             GaussianKernel kernel = new GaussianKernel(test.Bandwidth);
             ClusterAlgorithms.MeanShift<Vector2, Vector2Shape, GaussianKernel>(test.Input, kernel);
